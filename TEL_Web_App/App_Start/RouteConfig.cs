6c33201f43a2bbb7c18a6e0e375e4b9903e5d086
sajib49 <sajib49@gmail.com>
2016-06-22 11:58:43 +0600

    Link Problem Solved

--- TEL_Web_App/App_Start/RouteConfig.cs
+++ TEL_Web_App/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TEL_Web_App
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "LogIn", id = UrlParameter.Optional }
             );
         }
     }
